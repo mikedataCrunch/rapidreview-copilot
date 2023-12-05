1ec914e8d261cf854601e682c17cb4a303a35d8e
Ernest Toh <122539890+Ternestx@users.noreply.github.com>
2023-12-05 14:01:40 +0800

    Add files via upload

--- rrc/text_extraction.py
+++ rrc/text_extraction.py
@@ -7,6 +7,8 @@ from glob import glob
 import numpy as np
 import pandas as pd
 import os
+import uuid
+import re
 
 # extract text from pdfs
 import pdfplumber
@@ -35,6 +37,10 @@ class PDFExtractor():
         else:
             self.paths = glob(os.path.join(src_dir, '*'))
     
+    def generate_article_id(self):
+        # Generate a UUID-based article ID
+        return str(uuid.uuid4())
+
     def extract(self, extractor, path=None):
         """Extract PDF text from pdf filepath and returns extracted text in a json structure"""
         
@@ -46,21 +52,22 @@ class PDFExtractor():
             if not os.path.exists(full_path):
                 raise FileNotFoundError(f"The file at path '{full_path}' does not exist.")
             # include the choice of python pdf extract package to use
+
+            # Generate unique article id
+            article_id = self.generate_article_id()
+            extracted_text = ""
+            
             if extractor=='pdfplumber':
-                # List to store extracted text as dictionaries
-                extracted_text_list = []
                 # Extract the text
                 with pdfplumber.open(full_path) as pdf:
-                    for page_num, page in enumerate(pdf.pages, start=1):
+                    for page in pdf.pages:
                         # Get the bounding boxes of the tables on the page.
                         bboxes = [
                             table.bbox
                             for table in page.find_tables(
                                 table_settings={
-                                    "vertical_strategy": "explicit",
-                                    "horizontal_strategy": "explicit",
-                                    "explicit_vertical_lines": page.curves + page.edges,
-                                    "explicit_horizontal_lines": page.curves + page.edges,
+                                    "vertical_strategy": "lines",
+                                    "horizontal_strategy": "lines",
                                 }
                             )
                         ]
@@ -74,41 +81,35 @@ class PDFExtractor():
                                 return (h_mid >= x0) and (h_mid < x1) and (v_mid >= top) and (v_mid < bottom)
 
                             return not any(obj_in_bbox(__bbox) for __bbox in bboxes)
-
-                        # Extract text from the page
-                        extracted_text = page.filter(not_within_bboxes).extract_text()
-
-                        # Create a dictionary with "page" and "extracted text" keys
-                        page_dict = {"page": page_num, "extracted text": extracted_text}
                         
-                        # Append the dictionary to the list
-                        extracted_text_list.append(page_dict)
+                         # Extract text from the page and concatenate it
+                        page_text = page.filter(not_within_bboxes).extract_text()
+                        extracted_text += page_text
+
+                    # Create a dictionary with article id and "extracted text" keys
+                    output = {"article id": article_id, "extracted text": extracted_text}
 
                     # Return the list of dictionaries
-                    return extracted_text_list
+                    return output
                 
             elif extractor == 'PdfReader':
                 # Creating a pdf reader object
                 reader = PdfReader(full_path)
-                # List to store extracted text as dictionaries
-                extracted_text_list = []
 
                 # Loop through every page
-                for page_num in range(reader.numPages):
+                for page_num in range(len(reader.pages)):
+                    
                     # Get a specific page
-                    page = reader.getPage(page_num)
+                    page = reader.pages[page_num]
 
                     # Extract text from the page
-                    page_text = page.extract_text()
+                    extracted_text += re.sub("[\t\n\x0b\r\f]", ' ', page.extract_text()) + " "
 
-                    # Create a dictionary with "page" and "extracted text" keys
-                    page_dict = {"page": page_num + 1, "extracted text": page_text}
-
-                    # Append the dictionary to the list
-                    extracted_text_list.append(page_dict)
+                # Create a dictionary with "page" and "extracted text" keys
+                output = {"article id": article_id, "extracted text": extracted_text}
 
                 # Return the list of dictionaries
-                return extracted_text_list
+                return output
 
         except FileNotFoundError as e:
             return [{"error": str(e)}]
